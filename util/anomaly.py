026964d24d99cea5b222cf31c616aec1cbb3b744
jongjuny <95059601+jongjuny@users.noreply.github.com>
2024-06-14 01:34:00 -0400

    Add files via upload
    Revise the name 'sequential' to 'periodic'

--- util/anomaly.py
+++ util/anomaly.py
@@ -6,7 +6,7 @@ from scipy import signal
 
 # injects a random sequence of anomalies (i.e collective anomaly)
 class CollectiveAnomaly:
-    def __init__(self, length: int, percentage: float, distribution: str = 'uniform', upperbound: float = None, lowerbound: float = None, mu: float = None, std: float = None, skew=None, num_values: int = 5) -> None:
+    def __init__(self, length: int, percentage: float, distribution: str = 'uniform', upperbound: float = None, lowerbound: float = None, mu: float = None, sigma: float = None, skew=None, num_values: int = 5) -> None:
         # if upperbound and lowerbound not given, then random sequence anomaly can be generated using random values already present in the data
         self.upperbound = upperbound
         self.lowerbound = lowerbound
@@ -14,25 +14,25 @@ class CollectiveAnomaly:
         self.percentage = percentage
         self.dist = distribution
         self.mean = mu
-        self.std = std
+        self.std = sigma
         self.num_values = num_values
         self.skew = skew
 
 # injects point anomalies; these deviate significantly from the rest of the data
 class PointAnomaly:
-    def __init__(self, percentage: float, distribution: str = 'uniform', mu: float = None, std: float = None, num_values: int = 5, lowerbound=None, upperbound=None, skew=None) -> None:
+    def __init__(self, percentage: float, distribution: str = 'uniform', mu: float = None, sigma: float = None, num_values: int = 5, lowerbound=None, upperbound=None, skew=None) -> None:
         self.percentage = percentage
         self.dist = distribution
         self.mean = mu
-        self.std = std
+        self.std = sigma
         self.num_values = num_values
         self.skew = skew
         self.lowerbound = lowerbound
         self.upperbound = upperbound
 
 # injects sequential anomalies into the dataset (collective anomalies that keep repeating)
-class SequentialAnomaly:
-    def __init__(self, percentage: float, noise_factor: int, start=None, end=None, length=15):
+class PeriodicAnomaly:
+    def __init__(self, percentage: float, noise_factor: float, start=None, end=None, length=15):
         self.length = length
         self.percentage = percentage
         self.noise_factor = noise_factor
